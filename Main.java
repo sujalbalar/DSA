f222f167843e55ec621f9ae50d90b0607c40ded5
suju666 <sujalbalar02@gmail.com>
2024-07-22 23:08:46 +0530

    add implementatin of BST tree

---
+++ Main.java
@@ -0,0 +1,20 @@
+import tree.*;
+import static tree.traversal.Preorder.preorder;
+import static tree.traversal.Postorder.postorder;
+import static tree.traversal.Inorder.inorder;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {50,20,80,10,30,70,90};
+        BST.buildTree(arr);
+
+        System.out.println("\nPRE ORDER");
+        preorder(BST.HEAD);
+
+        System.out.println("\n\nIN ORDER");
+        inorder(BST.HEAD);
+
+        System.out.println("\n\nPOST ORDER");
+        postorder(BST.HEAD);
+    }
+}
---
+++ tree/BST.java
@@ -0,0 +1,40 @@
+package tree;
+
+public class BST {
+
+    public static Node HEAD;
+    static int index = 0;
+
+    public static class Node {
+        public int data;
+        public Node left, right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        } 
+    }
+
+    public static Node insert(Node tmp, int data){
+        if(tmp == null){
+            Node node = new Node(data);
+            if(HEAD == null)
+                HEAD = node;
+            return node;
+        }
+
+        if(data < tmp.data)
+            tmp.left = insert(tmp.left, data);
+        else
+            tmp.right = insert(tmp.right, data);
+        return tmp;
+    }
+
+    public static void buildTree(int[] arr){
+        while(index < arr.length){
+            insert(HEAD, arr[index++]);
+        }
+    }
+
+}
