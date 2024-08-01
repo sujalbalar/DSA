7edf2e7de3c6170e05f5a342906c6f4d231acf92
suju666 <sujalbalar02@gmail.com>
2024-08-01 21:50:42 +0530

    Doubly Link List

--- linked_list/doubly/Main.java
+++ linked_list/doubly/Main.java
@@ -2,22 +2,42 @@ package linked_list.doubly;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\nINSERT Operation");
+
         LList.addFirst(20);
         LList.addLast(30);
         LList.addLast(40);
-        LList.addLast(50);
+        LList.addLast(60);
         LList.addFirst(10);
 
         LList.print();
 
-        System.out.println("\n\nDELETE Operation\n");
+        System.out.println("\n\nDELETE Operation");
 
-        System.out.println("DELETE FIRST");
         LList.deleteFirst();
         LList.print();
 
-        System.out.println("\n\nDELETE LAST");
+        System.out.print("\n");
         LList.deleteLast();
+        LList.print();
+
+        System.out.println("\n\nDELETE By Node Value");
+        LList.delete(30);
+        LList.print();
+
+        System.out.println();
+
+        LList.addAt(30, 2);
+        LList.print();
+
+        System.out.println();
+
+        LList.addAt(0, 1);
+        LList.print();
+
+        System.out.println("\n INSERT Operation add at specific position");
+
+        LList.addAt(25, 3);
         LList.print();
     }
 }
