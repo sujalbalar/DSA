c0004bb7e261153fac92532ccdb98b4dd4c46ed0
suju666 <sujalbalar02@gmail.com>
2024-08-01 21:56:23 +0530

    insert, delete new now at specific position

--- linked_list/doubly/LList.java
+++ linked_list/doubly/LList.java
@@ -2,6 +2,7 @@ package linked_list.doubly;
 
 public class LList {
     public static Node Head;
+    public static int count;
 
     public static boolean isEmpty(){
         return Head == null;
@@ -9,6 +10,7 @@ public class LList {
 
     public static void addFirst(int data){
         Node node = new Node(data);
+        count++;
         if(isEmpty()){
             Head = node;
             return;
@@ -26,6 +28,7 @@ public class LList {
             return;
         }
 
+        count++;
         Node tmp = Head;
         while(tmp.next != null){
             tmp = tmp.next;
@@ -35,15 +38,42 @@ public class LList {
         tmp.next = node;
     }
 
+    public static void addAt(int data, int pos){
+        int current = 1;
+
+        if(isEmpty() || pos == 1){
+            addFirst(data);
+        }
+        else{
+            Node node = new Node(data);
+            Node tmp = Head;
+            Node previous = tmp.previous;
+
+            if(pos <= count) {
+                while (current < pos && tmp.next != null) {
+                    tmp = tmp.next;
+                    previous = tmp.previous;
+                    current++;
+                }
+
+                previous.next = node;
+                node.previous = previous;
+                node.next = tmp;
+                tmp.previous = node;
+                count++;
+            }
+        }
+    }
+
     public static void deleteFirst(){
         if(!isEmpty()){
             Head = Head.next;
             Head.previous = null;
+            count--;
         }
     }
 
     public static void deleteLast(){
-
         if(!isEmpty()){
             Node secondLast = Head;
             Node last = Head.next;
@@ -55,6 +85,34 @@ public class LList {
 
             secondLast.next = null;
             last.previous = null;
+            count--;
+        }
+    }
+
+    public static void delete(int data){
+        if(!isEmpty()){
+            if(Head.data == data){
+                deleteFirst();
+                return;
+            }
+
+            Node tmp = Head.next;
+            Node previous = tmp.previous;
+            Node next = tmp.next;
+
+            while(tmp.next != null){
+                if(tmp.data != data) {
+                    tmp = tmp.next;
+                    previous = tmp.previous;
+                    next = tmp.next;
+                }
+                else
+                    break;
+            }
+            previous.next = tmp.next;
+            if(next != null)
+                next.previous = tmp.previous;
+            count--;
         }
     }
 
